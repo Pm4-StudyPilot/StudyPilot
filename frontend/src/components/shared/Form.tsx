1b7a412f9424b89d603dd2b3c13e7e52ba5f0371
Annie Hemprich <hemprann@students.zhaw.ch>
2026-03-25 11:48:09 +0100

    Add a form component that wraps form elements

---
+++ frontend/src/components/shared/Form.tsx
@@ -0,0 +1,15 @@
+import { FormHTMLAttributes } from "react";
+
+interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
+  loading?: boolean
+  error?: string
+}
+
+export default function Form({ children, loading, error, ...props }: FormProps) {
+  return (
+    <form {...props}>
+      {error && <div className="alert alert-danger">{error}</div>}
+      {children}
+    </form>
+  );
+}
