8c40b209eef88966fe5e2692cdb0bbe0aa456f5a
Annie Hemprich <hemprann@students.zhaw.ch>
2026-03-25 15:11:12 +0100

    Adjust the progress bar to be able to handle edge cases

--- frontend/src/components/shared/ProgressBar.tsx
+++ frontend/src/components/shared/ProgressBar.tsx
@@ -1,10 +1,13 @@
 export default function ProgressBar({ value }:{value:number}) {
+    const intValue = Math.min(3, Math.max(0, Math.round(value)))
+    const floatValue = Math.min(3, Math.max(0, value))
     return (
     <div className="mb-2">
         <div className={`progress`} style={{ marginTop: '-0.5rem', height: '0.5rem'}}>
             <div
-            className={`progress-bar bg-${["danger", "warning", "info", "success"][value]}`}
-            style={{ width: `${(value / 4) * 100}%` }}
+            role="progressbar"
+            className={`progress-bar bg-${["danger", "warning", "info", "success"][intValue]}`}
+            style={{ width: `${(floatValue / 4) * 100}%` }}
             />
         </div>
     </div>
