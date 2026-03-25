37cab5d60b179130fdaaf982cca5ed67bddef744
Annie Hemprich <hemprann@students.zhaw.ch>
2026-03-25 11:55:32 +0100

    Add a textare field component with form error handling.

---
+++ frontend/src/components/shared/TextareaField.tsx
@@ -0,0 +1,17 @@
+import { TextareaHTMLAttributes, useId } from "react"
+import { FormFieldLayout, BaseProps } from "./FormFieldLayout"
+
+type TextareaFieldProps = BaseProps &
+  TextareaHTMLAttributes<HTMLTextAreaElement>
+
+export default function TextareaField({ error, id, ...props }: TextareaFieldProps) {
+const finalId = id ?? useId()
+  return (
+    <FormFieldLayout {...props} id={finalId} error={error}>
+      <textarea
+        {...props}
+        className={`form-control${error ? " is-invalid" : ""}`}
+      />
+    </FormFieldLayout>
+  )
+}
