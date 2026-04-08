be31f70d12c72b5db67effe10858eaba63412ed9
Vibishek Rathirajan <rathivib@students.zhaw.ch>
2026-04-08 21:55:57 +0200

    Add CourseList to HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,21 +1,14 @@
-import { useAuth } from '../context/AuthContext';
 import Navbar from '../components/shared/layout/Navbar';
-import UploadButton from '../components/shared/UploadButton';
+import CourseList from '../components/courses/CourseList';
 
 export default function HomePage() {
-  const { user } = useAuth();
-
   return (
     <>
       <Navbar />
       <div className="container mt-4">
-        <h1>Welcome, {user?.username}!</h1>
-        <p className="lead">Your study dashboard will appear here.</p>
-
-        <div className="card mt-4" style={{ maxWidth: 400 }}>
-          <div className="card-body">
-            <h5 className="card-title">Test Upload</h5>
-            <UploadButton bucket="test">Upload Asset</UploadButton>
+        <div className="row">
+          <div className="col-12 col-lg-7">
+            <CourseList />
           </div>
         </div>
       </div>
