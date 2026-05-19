bcfae83ba32e91ed4d38bf98649e0b37ecdd31ad
Annie Hemprich <78167294+Avvvvvvie@users.noreply.github.com>
2026-05-19 21:21:01 +0200

    Style checkbox component

--- frontend/src/components/shared/form/FormFieldLayout.tsx
+++ frontend/src/components/shared/form/FormFieldLayout.tsx
@@ -1,15 +1,16 @@
-import { ReactNode } from "react"
+import { ReactNode } from 'react';
 
 type FormFieldLayoutProps = {
-  label: string
-  error?: string
-  description?: string
-  iconLeft?: ReactNode
-  iconRight?: ReactNode
-  noMargin?: boolean
-  id?: string
-  children: ReactNode
-}
+  label: string;
+  error?: string;
+  description?: string;
+  iconLeft?: ReactNode;
+  iconRight?: ReactNode;
+  noMargin?: boolean;
+  id?: string;
+  children: ReactNode;
+  className?: string;
+};
 
 export function FormFieldLayout({
   label,
@@ -19,11 +20,16 @@ export function FormFieldLayout({
   iconRight,
   children,
   noMargin,
-  id
+  id,
+  className = '',
 }: FormFieldLayoutProps) {
   return (
-    <div className={noMargin ? '' : 'mb-3'}>
-      {id && (<label className="form-label" htmlFor={id}>{label}</label>)}
+    <div className={noMargin ? className : 'mb-3 ' + className}>
+      {id && (
+        <label className="form-label" htmlFor={id}>
+          {label}
+        </label>
+      )}
       <div role="group" className="input-group">
         {iconLeft && <span className="input-group-text">{iconLeft}</span>}
         {children}
@@ -33,5 +39,5 @@ export function FormFieldLayout({
       {description && <div className="form-text">{description}</div>}
       {error && <div className="invalid-feedback d-block">{error}</div>}
     </div>
-  )
+  );
 }
--- frontend/src/components/shared/form/InputField.tsx
+++ frontend/src/components/shared/form/InputField.tsx
@@ -1,27 +1,27 @@
-import { InputHTMLAttributes, useId } from "react"
-import { FormFieldLayout } from "./FormFieldLayout"
+import { InputHTMLAttributes, useId } from 'react';
+import { FormFieldLayout } from './FormFieldLayout';
 
 type InputFieldProps = {
-  label: string
-  error?: string
-} & InputHTMLAttributes<HTMLInputElement>
+  label: string;
+  error?: string;
+} & InputHTMLAttributes<HTMLInputElement>;
 
-export default function InputField({
-  error,
-  id,
-  label,
-  ...inputProps
-}: InputFieldProps) {
-  const generatedId = useId()
-  const finalId = id ?? generatedId
+export default function InputField({ error, id, label, ...inputProps }: InputFieldProps) {
+  const generatedId = useId();
+  const finalId = id ?? generatedId;
 
   return (
-    <FormFieldLayout label={label} error={error} id={finalId}>
+    <FormFieldLayout
+      label={label}
+      error={error}
+      id={finalId}
+      className={inputProps.type === 'checkbox' ? 'form-check-layout' : ''}
+    >
       <input
         {...inputProps}
         id={finalId}
-        className={`form-control${error ? " is-invalid" : ""}`}
+        className={`form-control${error ? ' is-invalid' : ''}${inputProps.type === 'checkbox' ? ' form-check-input' : ''}`}
       />
     </FormFieldLayout>
-  )
+  );
 }
